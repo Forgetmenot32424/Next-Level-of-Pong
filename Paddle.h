ca55a51197c1ab5c64eeba44a412f476f04af562
Forgetmenot32424 <antoni.horemski@student.put.poznan.pl>
2025-09-17 06:29:46 +0200

    FIRST MILESTONE - Końcowa Wersja Gry
    W końcowej wersji gry poza naprawą kilku występujących we wcześniejszych wersjach błędów dodane zostały następujące funkcjonalności:
    
    1. Power-upy, które urozmaicają rozgrywkę i pozwalają graczom na szybką zmianę dynamiki meczu a w tym:
    
    - Power-up prędkości zwiększający na 10 sekund prędkość poruszania się rakietki. Można go aktywować za pomocą lewego shifta w przypadku lewego gracza i za pomocą prawego ctrl w przypadku prawego gracza. Power-up po użyciu przechodzi w tryb "cooldown" na 25 sekund podczas, których nie można go użyć.
    
    -Power-up rozmiaru zwiększający na 10 sekund rozmiar rakietki. Można go aktywować za pomocą przycisku space w przypadku lewego gracza i za pomocą prawego shifta w przypadku prawego gracza. Power-up po użyciu przechodzi w tryb "cooldown" na 25 sekund podczas, których nie można go użyć.
    
    1.1 Ikony power-upów
    Dodane zostały także ikony, które informują gracza o dostępności danej umiejętności. Gdy power-up jest gotowy do użycia – ikona jest w pełni widoczna. Gdy trwa cooldown – ikona staje się wyraźnie przyciemniona, aż do momentu ponownego odblokowania umiejętności.
    
    2. Menu gry
    Dodane zostało menu startowe, w którym wyświetla się nazwa gry oraz  informacja o możliwości rozpoczęcia rozgrywki poprzez naciśnięcie klawisza Enter.
    
    3.Ekran zwycięstwa
    Po zdobyciu przez jednego z graczy 5 punktów wyświetla się komunikat informujący o zwycięstwie. Po zakończeniu meczu istnieje możliwość ponownego uruchomienia gry za pomocą przycisku Enter.

--- Paddle.h
+++ Paddle.h
@@ -6,34 +6,91 @@
 class Paddle {
 public:
     sf::RectangleShape shape;
-    float speed;
 
     Paddle(float x, float y) {
-        shape.setSize(sf::Vector2f(12.f, 100.f));
-        shape.setFillColor(sf::Color::White);
-        shape.setOrigin(6.f, 50.f);
+        shape.setSize(sf::Vector2f(WIDTH_PX, HEIGHT_PX));
+        shape.setOrigin(shape.getSize().x * 0.5f, shape.getSize().y * 0.5f);
         shape.setPosition(x, y);
-        speed = 8.f;
+        shape.setFillColor(sf::Color::White);
+
+        currentSpeed = BASE_SPEED;
     }
 
-    void moveUp() {
-        shape.move(0.f, -speed);
-    }
-
-    void moveDown() {
-        shape.move(0.f, speed);
-    }
+    void moveUp()   { shape.move(0.f, -currentSpeed); }
+    void moveDown() { shape.move(0.f,  currentSpeed); }
 
     void update(int windowHeight) {
-        float halfHeight = shape.getSize().y * 0.5f;
+        float halfH = shape.getSize().y * 0.5f;
         sf::Vector2f pos = shape.getPosition();
-
-        if (pos.y - halfHeight < 0.f)
-            pos.y = halfHeight;
-        if (pos.y + halfHeight > windowHeight)
-            pos.y = windowHeight - halfHeight;
-
+        if (pos.y - halfH < 0.f)          pos.y = halfH;
+        if (pos.y + halfH > windowHeight) pos.y = windowHeight - halfH;
         shape.setPosition(pos);
+        if (sizeActive && sizeClock.getElapsedTime().asSeconds() > POWER_DURATION_S) {
+            deactivateSizePowerUp();
+            sizeCooldownClock.restart();
+        }
+        if (speedActive && speedClock.getElapsedTime().asSeconds() > POWER_DURATION_S) {
+            deactivateSpeedPowerUp();
+            speedCooldownClock.restart();
+        }
+
+        if (!canUseSize && !sizeActive && sizeCooldownClock.getElapsedTime().asSeconds() > COOLDOWN_S) {
+            canUseSize = true;
+        }
+        if (!canUseSpeed && !speedActive && speedCooldownClock.getElapsedTime().asSeconds() > COOLDOWN_S) {
+            canUseSpeed = true;
+        }
+    }
+
+    void activateSizePowerUp() {
+        if (!sizeActive && canUseSize) {
+            shape.setSize(sf::Vector2f(WIDTH_PX, HEIGHT_PX * 2.f));
+            shape.setOrigin(shape.getSize().x * 0.5f, shape.getSize().y * 0.5f);
+            sizeClock.restart();
+            sizeActive = true;
+            canUseSize = false;
+        }
+    }
+    bool isSizeReady() const { return canUseSize; }
+
+    void activateSpeedPowerUp() {
+        if (!speedActive && canUseSpeed) {
+            currentSpeed = BASE_SPEED * SPEED_MULT;
+            speedClock.restart();
+            speedActive = true;
+            canUseSpeed = false;
+        }
+    }
+    bool isSpeedReady() const { return canUseSpeed; }
+
+private:
+    static constexpr float POWER_DURATION_S = 10.f;
+    static constexpr float COOLDOWN_S       = 25.f;
+    static constexpr float WIDTH_PX         = 20.f;
+    static constexpr float HEIGHT_PX        = 100.f;
+    static constexpr float SPEED_MULT       = 2.f;
+    static constexpr float BASE_SPEED       = 8.f;
+
+    bool      sizeActive = false;
+    bool      canUseSize = true;
+    sf::Clock sizeClock;
+    sf::Clock sizeCooldownClock;
+
+    bool      speedActive = false;
+    bool      canUseSpeed = true;
+    sf::Clock speedClock;
+    sf::Clock speedCooldownClock;
+
+    float currentSpeed = BASE_SPEED;
+
+    void deactivateSizePowerUp() {
+        shape.setSize(sf::Vector2f(WIDTH_PX, HEIGHT_PX));
+        shape.setOrigin(shape.getSize().x * 0.5f, shape.getSize().y * 0.5f);
+        sizeActive = false;
+    }
+    void deactivateSpeedPowerUp() {
+        currentSpeed = BASE_SPEED;
+        speedActive = false;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 #include "Sound.h"
 #include "Theme.h"
 
+static void centerText(sf::Text& t, float cx, float cy) {
+    sf::FloatRect b = t.getLocalBounds();
+    t.setOrigin(b.left + b.width * 0.5f, b.top + b.height * 0.5f);
+    t.setPosition(cx, cy);
+}
+
+enum class GameState { Menu, Playing, GameOver };
+
 int main()
 {
     const int WIDTH = 1920;
@@ -31,6 +39,54 @@ int main()
     int scoreRight = 0;
     Score score(WIDTH, HEIGHT);
 
+    sf::Texture powerTexSize, powerTexSpeed;
+    powerTexSize.loadFromFile ("C:/Users/antek/OneDrive/Dokumenty/NEXT_LEVEL_OF_PONG/Resources/powerup.png");
+    powerTexSpeed.loadFromFile("C:/Users/antek/OneDrive/Dokumenty/NEXT_LEVEL_OF_PONG/Resources/powerup2.png");
+
+    sf::Sprite leftIconSize(powerTexSize), rightIconSize(powerTexSize);
+    leftIconSize.setScale(0.5f, 0.5f);
+    rightIconSize.setScale(0.5f, 0.5f);
+    leftIconSize.setPosition(50.f, 20.f);
+    rightIconSize.setPosition(WIDTH - 50.f - powerTexSize.getSize().x * 0.5f, 20.f);
+
+    sf::Sprite leftIconSpeed(powerTexSpeed), rightIconSpeed(powerTexSpeed);
+    leftIconSpeed.setScale(0.11f, 0.11f);
+    rightIconSpeed.setScale(0.11f, 0.11f);
+    float marginBottom = 10.f;
+    float leftSpeedY  = HEIGHT - marginBottom - powerTexSpeed.getSize().y * 0.11f;
+    float rightSpeedY = HEIGHT - marginBottom - powerTexSpeed.getSize().y * 0.11f;
+    leftIconSpeed.setPosition(leftIconSize.getPosition().x, leftSpeedY);
+    rightIconSpeed.setPosition(rightIconSize.getPosition().x, rightSpeedY);
+
+    sf::Font font;
+    font.loadFromFile("C:/Users/antek/OneDrive/Dokumenty/NEXT_LEVEL_OF_PONG/Resources/Sakamoto.ttf");
+
+
+    sf::Text title("NEXT LEVEL OF PONG", font, 100);
+    sf::Text startMsg("Press ENTER to Start", font, 50);
+    centerText(title, WIDTH * 0.5f, HEIGHT * 0.5f - 100.f);
+    centerText(startMsg, WIDTH * 0.5f, HEIGHT * 0.5f + 50.f);
+
+    sf::Text winText;
+    winText.setFont(font);
+    winText.setCharacterSize(120);
+    winText.setFillColor(sf::Color::Yellow);
+    winText.setStyle(sf::Text::Bold);
+
+    GameState state = GameState::Menu;
+
+    auto resetGame = [&]() {
+        scoreLeft = 0;
+        scoreRight = 0;
+
+        leftPaddle  = Paddle(30.f, HEIGHT / 2.f);
+        rightPaddle = Paddle(WIDTH - 30.f, HEIGHT / 2.f);
+        wall        = Wall(WIDTH / 2.f, HEIGHT / 2.f);
+
+        ball.reset();
+        ball.shape.setPosition(WIDTH / 2.f, HEIGHT / 2.f);
+    };
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -38,25 +94,54 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed &&
-                event.key.code == sf::Keyboard::Escape)
-                window.close();
+
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+
+                if (state == GameState::Menu && event.key.code == sf::Keyboard::Enter) {
+                    state = GameState::Playing;
+                }
+                else if (state == GameState::Playing) {
+                    if (event.key.code == sf::Keyboard::Space)
+                        leftPaddle.activateSizePowerUp();
+                    if (event.key.code == sf::Keyboard::RShift)
+                        rightPaddle.activateSizePowerUp();
+                    if (event.key.code == sf::Keyboard::LShift)
+                        leftPaddle.activateSpeedPowerUp();
+                    if (event.key.code == sf::Keyboard::RControl)
+                        rightPaddle.activateSpeedPowerUp();
+                }
+                else if (state == GameState::GameOver && event.key.code == sf::Keyboard::Enter) {
+                    resetGame();
+                    state = GameState::Playing;
+                }
+            }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) leftPaddle.moveUp();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) leftPaddle.moveDown();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) rightPaddle.moveUp();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) rightPaddle.moveDown();
+        if (state == GameState::Playing) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))    leftPaddle.moveUp();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))    leftPaddle.moveDown();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))   rightPaddle.moveUp();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) rightPaddle.moveDown();
 
-        leftPaddle.update(HEIGHT);
-        rightPaddle.update(HEIGHT);
-        wall.update(HEIGHT);
+            leftPaddle.update(HEIGHT);
+            rightPaddle.update(HEIGHT);
+            wall.update(HEIGHT);
+            ball.update(WIDTH, HEIGHT, leftPaddle, rightPaddle, wall, scoreLeft, scoreRight, sfx);
 
-        ball.update(WIDTH, HEIGHT, leftPaddle, rightPaddle, wall, scoreLeft, scoreRight, sfx);
-
+            if (scoreLeft >= 5) {
+                winText.setString("PLAYER 1 WINS!");
+                centerText(winText, WIDTH * 0.5f, HEIGHT * 0.5f);
+                state = GameState::GameOver;
+            } else if (scoreRight >= 5) {
+                winText.setString("PLAYER 2 WINS!");
+                centerText(winText, WIDTH * 0.5f, HEIGHT * 0.5f);
+                state = GameState::GameOver;
+            }
+        }
         int themeIndex = ThemeIndexFromSteps(ball.bounces);
         const auto& theme = AllThemes()[themeIndex];
-
         leftPaddle.shape.setFillColor(theme.paddle);
         rightPaddle.shape.setFillColor(theme.paddle);
         wall.shape.setFillColor(theme.wall);
@@ -66,13 +151,33 @@ int main()
 
         score.update(scoreLeft, scoreRight, ball.bounces);
 
+        leftIconSize.setColor (leftPaddle.isSizeReady()    ? sf::Color(255,255,255,255) : sf::Color(255,255,255,100));
+        rightIconSize.setColor(rightPaddle.isSizeReady()   ? sf::Color(255,255,255,255) : sf::Color(255,255,255,100));
+        leftIconSpeed.setColor (leftPaddle.isSpeedReady()  ? sf::Color(255,255,255,255) : sf::Color(255,255,255,100));
+        rightIconSpeed.setColor(rightPaddle.isSpeedReady() ? sf::Color(255,255,255,255) : sf::Color(255,255,255,100));
+
         window.clear();
-        window.draw(bg);
-        window.draw(leftPaddle.shape);
-        window.draw(rightPaddle.shape);
-        window.draw(wall.shape);
-        window.draw(ball.shape);
-        score.draw(window);
+
+        if (state == GameState::Menu) {
+            window.draw(title);
+            window.draw(startMsg);
+        }
+        else if (state == GameState::Playing) {
+            window.draw(bg);
+            window.draw(leftPaddle.shape);
+            window.draw(rightPaddle.shape);
+            window.draw(wall.shape);
+            window.draw(ball.shape);
+            score.draw(window);
+            window.draw(leftIconSize);
+            window.draw(rightIconSize);
+            window.draw(leftIconSpeed);
+            window.draw(rightIconSpeed);
+        }
+        else if (state == GameState::GameOver) {
+            window.draw(winText);
+        }
+
         window.display();
     }
 
